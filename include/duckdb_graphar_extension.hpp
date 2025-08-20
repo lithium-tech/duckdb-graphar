a0c591df49d5aba380826d7416708d3d86fb4f56
Andrey <andrjob@gmail.com>
2025-08-20 08:21:33 +0300

    m: unified paths and names (#6)
    Co-authored-by: Andrey Berezin <a.v.berezin@tbank.ru>

--- include/duckdb_graphar_extension.hpp
+++ include/duckdb_graphar_extension.hpp
@@ -4,7 +4,7 @@
 
 namespace duckdb {
 
-class GrapharDuckExtension : public Extension {
+class DuckdbGrapharExtension : public Extension {
 public:
     void Load(DuckDB& db) override;
     std::string Name() override;
--- src/duckdb_graphar_extension.cpp
+++ src/duckdb_graphar_extension.cpp
@@ -1,6 +1,6 @@
 #define DUCKDB_EXTENSION_MAIN
 
-#include "graphar_duck_extension.hpp"
+#include "duckdb_graphar_extension.hpp"
 
 #include "functions/scalar/bfs.hpp"
 #include "functions/table/edges_vertex.hpp"
@@ -20,18 +20,18 @@
 
 namespace duckdb {
 
-inline void GraphArDuckScalarFun(DataChunk& args, ExpressionState& state, Vector& result) {
+inline void DuckdbGrapharScalarFun(DataChunk& args, ExpressionState& state, Vector& result) {
     auto& name_vector = args.data[0];
     UnaryExecutor::Execute<string_t, string_t>(name_vector, result, args.size(), [&](string_t name) {
-        return StringVector::AddString(result, "GraphArDuck " + name.GetString() + " 🐥");
+        return StringVector::AddString(result, "DuckDB_Graphar " + name.GetString() + " 🐥");
     });
 }
 
 static void LoadInternal(DatabaseInstance& instance) {
     // Register a scalar function
-    auto graphar_duck_scalar_function =
-        ScalarFunction("graphar_duck", {LogicalType::VARCHAR}, LogicalType::VARCHAR, GraphArDuckScalarFun);
-    ExtensionUtil::RegisterFunction(instance, graphar_duck_scalar_function);
+    auto duckdb_graphar_scalar_function =
+        ScalarFunction("duckdb_graphar", {LogicalType::VARCHAR}, LogicalType::VARCHAR, DuckdbGrapharScalarFun);
+    ExtensionUtil::RegisterFunction(instance, duckdb_graphar_scalar_function);
 
     auto& config = DBConfig::GetConfig(instance);
 
@@ -47,15 +47,15 @@ static void LoadInternal(DatabaseInstance& instance) {
     TwoHop::Register(instance);
     OneMoreHop::Register(instance);
 
-    config.storage_extensions["graphar_duck"] = make_uniq<GraphArStorageExtension>();
+    config.storage_extensions["duckdb_graphar"] = make_uniq<GraphArStorageExtension>();
 }
 
-void GrapharDuckExtension::Load(DuckDB& db) { LoadInternal(*db.instance); }
-std::string GrapharDuckExtension::Name() { return "graphar_duck"; }
+void DuckdbGrapharExtension::Load(DuckDB& db) { LoadInternal(*db.instance); }
+std::string DuckdbGrapharExtension::Name() { return "duckdb_graphar"; }
 
-std::string GrapharDuckExtension::Version() const {
-#ifdef EXT_VERSION_GRAPHAR_DUCK
-    return EXT_VERSION_GRAPHAR_DUCK;
+std::string DuckdbGrapharExtension::Version() const {
+#ifdef EXT_VERSION_DUCKDB_GRAPHAR
+    return EXT_VERSION_DUCKDB_GRAPHAR;
 #else
     return "";
 #endif
@@ -65,12 +65,12 @@ std::string GrapharDuckExtension::Version() const {
 
 extern "C" {
 
-DUCKDB_EXTENSION_API void graphar_duck_init(duckdb::DatabaseInstance& db) {
+DUCKDB_EXTENSION_API void duckdb_graphar_init(duckdb::DatabaseInstance& db) {
     duckdb::DuckDB db_wrapper(db);
-    db_wrapper.LoadExtension<duckdb::GrapharDuckExtension>();
+    db_wrapper.LoadExtension<duckdb::DuckdbGrapharExtension>();
 }
 
-DUCKDB_EXTENSION_API const char* graphar_duck_version() { return duckdb::DuckDB::LibraryVersion(); }
+DUCKDB_EXTENSION_API const char* duckdb_graphar_version() { return duckdb::DuckDB::LibraryVersion(); }
 }
 
 #ifndef DUCKDB_EXTENSION_MAIN
