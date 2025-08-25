17328ed44145fa313b37f98221cc7b044d58eda6
p.baderik <p.baderik@tbank.ru>
2025-08-25 16:24:38 +1000

    Add build and run tests
    Add run tests in Pipeline

--- config/extension_config.cmake
+++ config/extension_config.cmake
@@ -5,4 +5,5 @@ duckdb_extension_load(duckdb_graphar
     SOURCE_DIR "${EXTENSION_ROOT_DIR}/config"
     INCLUDE_DIR "${EXTENSION_ROOT_DIR}/include"
     LINKED_LIBS "arrow;graphar"
+    LOAD_TESTS
 )
