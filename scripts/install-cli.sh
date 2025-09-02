c9a856e5f002aa2cc86858425af47a9836e24ac7
p.baderik <p.baderik@tbank.ru>
2025-09-02 17:18:20 +1000

    m: Fix shared libs runpath

--- scripts/install-cli.sh
+++ scripts/install-cli.sh
@@ -6,4 +6,5 @@ pip3 install ./build/_deps/graphar-prefix/src/graphar/cli \
   --config-settings=cmake.define.ArrowDataset_DIR="$ROOTDIR/build/_deps/arrow-install/lib/cmake/ArrowDataset" \
   --config-settings=cmake.define.ArrowAcero_DIR="$ROOTDIR/build/_deps/arrow-install/lib/cmake/ArrowAcero" \
   --config-settings=cmake.define.Protobuf_INCLUDE_DIR="$ROOTDIR/build/_deps/arrow-prefix/src/arrow-build/protobuf_ep-install/include" \
-  --config-settings=cmake.define.Protobuf_LIBRARIES="$ROOTDIR/build/_deps/arrow-prefix/src/arrow-build/protobuf_ep-install/lib/libprotobuf.a"
+  --config-settings=cmake.define.Protobuf_LIBRARIES="$ROOTDIR/build/_deps/arrow-prefix/src/arrow-build/protobuf_ep-install/lib/libprotobuf.a" \
+  --config-settings=cmake.define.CMAKE_SHARED_LINKER_FLAGS="-Wl,--disable-new-dtags"
