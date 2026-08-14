6a17de5eb9d9609c54d76226e5dc0405eec5577d
p.baderik <p.baderik@tbank.ru>
2026-08-14 15:28:30 +1000

    feat(ci): fix cmake for build on first try

--- extension_config.cmake
+++ extension_config.cmake
@@ -1,8 +1,13 @@
 # Configuration for the DuckDB 'duckdb_graphar' extension.
 #
+if(NOT DEFINED EXTENSION_ROOT_DIR)
+    set(LOAD_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}")
+else()
+    set(LOAD_INCLUDE_DIR "${EXTENSION_ROOT_DIR}")
+endif()
 
 duckdb_extension_load(duckdb_graphar
-    INCLUDE_DIR "${EXTENSION_ROOT_DIR}/include"
+    INCLUDE_DIR "${LOAD_INCLUDE_DIR}/include"
     SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}"
     LOAD_TESTS
 )
